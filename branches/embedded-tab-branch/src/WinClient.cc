a11bf161dfcc80282d7aaf0e4c7274dbfb212725
fluxgen <fluxgen@54ec5f11-9ae8-0310-9a2b-99d706b22625>
2003-04-04 15:15:27 +0000

    holds client info

---
+++ branches/embedded-tab-branch/src/WinClient.cc
@@ -0,0 +1,216 @@
+// WinClient.cc for Fluxbox - an X11 Window manager
+// Copyright (c) 2003 Henrik Kinnunen (fluxgen(at)users.sourceforge.net)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// $Id: WinClient.cc,v 1.1.2.1 2003/04/04 15:15:27 fluxgen Exp $
+
+#include "WinClient.hh"
+
+#include "Window.hh"
+#include "fluxbox.hh"
+
+#include <iostream>
+using namespace std;
+
+WinClient::WinClient(Window win, FluxboxWindow &fbwin):FbTk::FbWindow(win),
+                     transient_for(0),
+                     window_group(0),
+                     title(""), icon_title(""),
+                     x(0), y(0), old_bw(0),
+                     width(1), height(1), 
+                     min_width(1), min_height(1),
+                     max_width(1), max_height(1),
+                     width_inc(1), height_inc(1),
+                     min_aspect_x(1), min_aspect_y(1),
+                     max_aspect_x(1), max_aspect_y(1),
+                     base_width(1), base_height(1),
+                     win_gravity(0),
+                     initial_state(0),
+                     normal_hint_flags(0),
+                     wm_hint_flags(0),
+                     mwm_hint(0),
+                     blackbox_hint(0),
+                     m_win(&fbwin),
+                     m_diesig(*this) { }
+
+WinClient::~WinClient() {
+#ifdef DEBUG
+    cerr<<__FILE__<<"("<<__FUNCTION__<<")"<<endl;
+#endif // DEBUG
+    m_diesig.notify();
+
+    Fluxbox *fluxbox = Fluxbox::instance();
+    if (mwm_hint != 0)
+        XFree(mwm_hint);
+
+    if (blackbox_hint != 0)
+        XFree(blackbox_hint);
+
+    if (window())
+        fluxbox->removeWindowSearch(window());
+
+    if (transient_for != 0) {
+        if (transient_for == m_win) {
+            transient_for = 0;
+        }
+
+        fluxbox->setFocusedWindow(transient_for);
+
+        if (transient_for) {
+            transient_for->m_client->transients.remove(m_win);			
+            transient_for->setInputFocus();
+            transient_for = 0;
+        }
+    }
+	
+    while (!transients.empty()) {
+        transients.back()->m_client->transient_for = 0;
+        transients.pop_back();
+    }
+	
+    if (window_group) {
+        fluxbox->removeGroupSearch(window_group);
+        window_group = 0;
+    }
+}
+
+void WinClient::updateRect(int x, int y, 
+                        unsigned int width, unsigned int height) {
+    Display *disp = FbTk::App::instance()->display();
+    XEvent event;
+    event.type = ConfigureNotify;
+
+    event.xconfigure.display = disp;
+    event.xconfigure.event = window();
+    event.xconfigure.window = window();
+    event.xconfigure.x = x;
+    event.xconfigure.y = y;
+    event.xconfigure.width = width;
+    event.xconfigure.height = height;
+    //!! TODO
+    event.xconfigure.border_width = 1;//client.old_bw;
+    //!! TODO
+    event.xconfigure.above = None; //m_frame.window().window();
+    event.xconfigure.override_redirect = false;
+
+    XSendEvent(disp, window(), False, StructureNotifyMask, &event);
+
+}
+
+void WinClient::sendFocus() {
+    Display *disp = FbTk::App::instance()->display();
+    // setup focus msg
+    XEvent ce;
+    ce.xclient.type = ClientMessage;
+    ce.xclient.message_type = FbAtoms::instance()->getWMProtocolsAtom();
+    ce.xclient.display = disp;
+    ce.xclient.window = window();
+    ce.xclient.format = 32;
+    ce.xclient.data.l[0] = FbAtoms::instance()->getWMTakeFocusAtom();
+    ce.xclient.data.l[1] = Fluxbox::instance()->getLastTime();
+    ce.xclient.data.l[2] = 0l;
+    ce.xclient.data.l[3] = 0l;
+    ce.xclient.data.l[4] = 0l;
+    // send focus msg
+    XSendEvent(disp, window(), false, NoEventMask, &ce);
+}
+
+void WinClient::sendClose() {
+    Display *disp = FbTk::App::instance()->display();
+    // fill in XClientMessage structure for delete message
+    XEvent ce;
+    ce.xclient.type = ClientMessage;
+    ce.xclient.message_type = FbAtoms::instance()->getWMProtocolsAtom();
+    ce.xclient.display = disp;
+    ce.xclient.window = window();
+    ce.xclient.format = 32;
+    ce.xclient.data.l[0] = FbAtoms::instance()->getWMDeleteAtom();
+    ce.xclient.data.l[1] = CurrentTime;
+    ce.xclient.data.l[2] = 0l;
+    ce.xclient.data.l[3] = 0l;
+    ce.xclient.data.l[4] = 0l;
+    // send event delete message to client window
+    XSendEvent(disp, window(), false, NoEventMask, &ce);
+}
+
+void WinClient::reparent(Window win, int x, int y) {
+    XReparentWindow(FbTk::App::instance()->display(), window(), win, x, y);
+}
+
+bool WinClient::getAttrib(XWindowAttributes &attr) const {
+    return XGetWindowAttributes(FbTk::App::instance()->display(), window(), &attr);
+}
+
+bool WinClient::getWMName(XTextProperty &textprop) const {
+    return XGetWMName(FbTk::App::instance()->display(), window(), &textprop);
+}
+
+bool WinClient::getWMIconName(XTextProperty &textprop) const {
+    return XGetWMName(FbTk::App::instance()->display(), window(), &textprop);
+}
+
+void WinClient::updateTransientInfo() {
+    if (m_win == 0)
+        return;
+    // remove us from parent
+    if (transientFor() != 0) {
+       transientFor()->m_client->transientList().remove(m_win);
+    }
+    
+    transient_for = 0;
+    Display *disp = FbTk::App::instance()->display();
+    // determine if this is a transient window
+    Window win;
+    if (!XGetTransientForHint(disp, window(), &win))
+        return;
+	
+    if (win == window())
+        return;
+	
+    if (win != 0 && Fluxbox::instance()->searchScreen(win) == 0) {
+        m_win->modal = true;
+        return;
+    }
+	
+    transient_for = Fluxbox::instance()->searchWindow(win);
+    if (transient_for != 0 &&
+        window_group != None && win == window_group) {		
+        transient_for = Fluxbox::instance()->searchGroup(win, m_win);
+    }
+	
+    // make sure we don't have deadlock loop in transient chain
+    for (FluxboxWindow *w = m_win; w != 0; w = w->m_client->transient_for) {
+        if (w == w->m_client->transient_for) {
+            w->m_client->transient_for = 0;
+#ifdef DEBUG	
+            cerr<<__FILE__<<"("<<__FUNCTION__<<"): w = transient_for"<<endl;
+#endif // DEBUG
+            break;
+        }
+    }
+	
+    if (transient_for != 0) {
+        transient_for->m_client->transients.push_back(m_win);
+        // make sure we only have on instance of this
+        transient_for->m_client->transients.unique(); 
+        m_win->stuck = transient_for->stuck;
+    }
+
+}
---
+++ branches/embedded-tab-branch/src/WinClient.hh
@@ -0,0 +1,105 @@
+// WinClient.hh for Fluxbox - an X11 Window manager
+// Copyright (c) 2003 Henrik Kinnunen (fluxgen(at)users.sourceforge.net)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+// $Id: WinClient.hh,v 1.1.2.1 2003/04/04 15:15:23 fluxgen Exp $
+
+#ifndef WINCLIENT_HH
+#define WINCLIENT_HH
+
+#include "BaseDisplay.hh"
+#include "Subject.hh"
+#include "FbWindow.hh"
+
+#include <X11/Xutil.h>
+#include <string>
+
+class FluxboxWindow;
+
+/// Holds client window info 
+class WinClient:public FbTk::FbWindow {
+public:
+    typedef std::list<FluxboxWindow *> TransientList;
+
+    WinClient(Window win, FluxboxWindow &fbwin);
+
+    ~WinClient();
+    void updateRect(int x, int y, unsigned int width, unsigned int height);
+    void sendFocus();
+    void sendClose();
+    void reparent(Window win, int x, int y);
+    bool getAttrib(XWindowAttributes &attr) const;
+    bool getWMName(XTextProperty &textprop) const;
+    bool getWMIconName(XTextProperty &textprop) const;
+
+    /// notifies when this client dies
+    FbTk::Subject &dieSig() { return m_diesig; }
+
+    /// updates transient window information
+    void updateTransientInfo();
+    FluxboxWindow *transientFor() { return transient_for; }
+    const FluxboxWindow *transientFor() const { return transient_for; }
+    TransientList &transientList() { return transients; }
+    const TransientList &transientList() const { return transients; }
+    bool operator == (const FluxboxWindow &win) const {
+        return (m_win == &win);
+    }
+
+    /**
+       !! TODO !!
+       remove or move these to private
+     */
+
+    FluxboxWindow *transient_for; // which window are we a transient for?
+    std::list<FluxboxWindow *> transients;  // which windows are our transients?
+    Window window_group;
+
+    std::string title, icon_title;
+    int x, y, old_bw;
+    unsigned int width, height,
+        min_width, min_height, max_width, max_height, width_inc, height_inc,
+        min_aspect_x, min_aspect_y, max_aspect_x, max_aspect_y,
+        base_width, base_height, win_gravity;
+    unsigned long initial_state, normal_hint_flags, wm_hint_flags;
+
+    // this structure only contains 3 elements... the Motif 2.0 structure contains
+    // 5... we only need the first 3... so that is all we will define
+    typedef struct MwmHints {
+        unsigned long flags;       // Motif wm flags
+        unsigned long functions;   // Motif wm functions
+        unsigned long decorations; // Motif wm decorations
+    } MwmHints;
+
+    MwmHints *mwm_hint;
+    BaseDisplay::BlackboxHints *blackbox_hint;
+    FluxboxWindow *m_win;
+    class WinClientSubj: public FbTk::Subject {
+    public:
+        explicit WinClientSubj(WinClient &client):m_winclient(client) { }
+        WinClient &winClient() { return m_winclient; }
+    private:
+        WinClient &m_winclient;
+    };
+
+private:
+    WinClientSubj m_diesig;
+};
+
+#endif // WINCLIENT_HH
